137a8a828937845b63f6eb37817acda89cbeb575
sonny <sonny@yaofangrui.local>
2016-03-09 17:31:30 +0800

    init
    初始化

---
+++ SSHeadEnlarge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SSHeadEnlarge
+//
+//  Created by sonny on 16/3/9.
+//  Copyright © 2016年 sonny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SSHeadEnlarge/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SSHeadEnlarge
+//
+//  Created by sonny on 16/3/9.
+//  Copyright © 2016年 sonny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
